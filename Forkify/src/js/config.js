7827e786fe93c0709c4d2341a3ab575d235ae9c4
arazdev <arazmyrat.dev@gmail.com>
2023-01-09 16:46:22 +0700

    Delete Forkify directory

--- Forkify/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '5be6d697-d074-4f20-9fb5-fc66a2746cf6';
-export const MODAL_CLOSE_SEC = 2.5;
